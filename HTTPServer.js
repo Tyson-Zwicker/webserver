682d48e2594405c63305c1daff7c0572aaf6fdf8
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-03-18 21:23:40 -0300

    updates

--- HTTPServer.js
+++ HTTPServer.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const child_process = require('child_process');
-const port = 2001;
+const port = 8080;
 const server = http.createServer(listener);
 server.listen(port);
 let verbose = 0;
@@ -11,29 +11,32 @@ if (process.argv.length === 3) {
   }
 }
 function listener(req, res) {
-  if (verbose) console.log(req.url)
+  let visitorsIP = ((req.headers['x-forwarded-for']?.split(',')[0].trim()) || req.socket.remoteAddress).substring(7);
+  if (verbose) {    
+    const now = new Date();
+    const stamp = `${String(now.getFullYear()).slice(-2)}${String(now.getMonth() + 1).padStart(2, '0')}${String(now.getDate()).padStart(2, '0')} ${String(now.getHours()).padStart(2, '0')}${String(now.getMinutes()).padStart(2, '0')}${String(now.getSeconds()).padStart(2, '0')}`;
+    console.log(stamp + '@' + visitorsIP + ' ' + req.url);
+  }
   if (req.method === 'GET') {
-    invokeWorker (req, res);
+    invokeWorker(req, res, '',visitorsIP);
   }
   if (req.method === 'POST') {
-    let body = '';
+    let postBody = '';
     // Listen for data chunks
     req.on('data', (chunk) => {
-      body += chunk.toString(); // Convert Buffer to string
+      postBody += chunk.toString(); // Convert Buffer to string
     });
 
     // Listen for the end of the request body
     req.on('end', () => {
-      invokeWorker (req, res,body);
+      invokeWorker(req, res, postBody, visitorsIP);
     });
   }
 }
-function invokeWorker(req,res,body) {
-  
-  if (!body) body='';
-  let workerVerbosity ='1';
-  var worker = child_process.fork(__dirname + '/HTTPWorker.js', [req.url, workerVerbosity,body]);
-  
+function invokeWorker(req, res, body, visitorIP) {
+  if (!body) body = '';
+  let workerVerbosity = '1';
+  var worker = child_process.fork(__dirname + '/HTTPWorker.js', [req.url, workerVerbosity, body, visitorIP]);
   worker.send(req.method);
   worker.on('message',
     (message) => {
@@ -42,10 +45,10 @@ function invokeWorker(req,res,body) {
       res.writeHead(msg.code);
       res.write(msg.data + '');
       res.end();
-      
-      try{        
+
+      try {
         process.kill(parseInt(worker.pid));
-      }catch (err){console.log ('SERVER could not kill worker process: '+worker.pid);}
+      } catch (err) { console.log('SERVER could not kill worker process: ' + worker.pid); }
     }
   );
 }
--- HTTPWorker.js
+++ HTTPWorker.js
@@ -4,40 +4,66 @@ const child_process = require('child_process');
 let verbose = 0;
 process.on('message', (message) => {
   let msg = message + '';
-  if (msg === 'SIGINT') {    
+  if (msg === 'SIGINT') {
     terminate();
   }
   let resource = process.argv[2];
   verbose = parseInt(process.argv[3]);
   let body = process.argv[4];
+  let visitorIP = process.argv[5];
+
+  let fullPath = process.argv[1].substring(0, process.argv[1].lastIndexOf('/'));
+  let startPath = process.argv[1].substring (0, process.argv[1].indexOf('/'));
+  console.log('DEBUG FULL Path: ' + fullPath);
+  console.log('DEBUG START Path: '+ startPath);
+  console.log('DEBUG Natural Resource: ' + resource);
 
   if (msg === 'GET') {
     if (resource.startsWith('/:')) {
-      let pathName = process.argv[1].substring(0, process.argv[1].lastIndexOf('/')) + '/services';
+      let pathName = fullPath + '/services';
       let serviceName = '/' + resource.substring(2) + '.js';
       if (verbose == 1) console.log('service-> ' + serviceName);
-      runService(pathName, serviceName);
+      runService(pathName, serviceName, 'GET');
     } else {
-      let pathName = process.argv[1].substring(0, process.argv[1].lastIndexOf('/')) + '/content';      
-      getFile(pathName, resource);
+      //--------------                        Special cases...                        ---This is where I fuck around with "hackers"..
+      if (fullPath === '/') {
+        getIndexPage();
+      }
+      //TODO: Review logs, and make a put all the script kiddies something do based on the startpath/resource they ask for...
+
+     /* else if (naturalPathName.startsWith === '/login') {
+        //Run a service for those who wish to login.
+        let serviceName = '/login.js';
+        if (verbose == 1) console.log('*service-> ' + serviceName);
+        runService(pathName, serviceName, body, 'POST');
+      }
+      else if (naturalPathName.startsWith === '/geoserver') {
+
+      }
+      */
+
+      else {
+        //Get the requested file.. if it exists...
+        let pathName = fullPath + '/content';
+        getFile(pathName, resource);
+      }
     }
   }
   if (msg === 'POST') {
     if (resource.startsWith('/:')) {
-      let pathName = process.argv[1].substring(0, process.argv[1].lastIndexOf('/')) + '/services';
+      let pathName = fullPath + '/services';
       let serviceName = '/' + resource.substring(2) + '.js';
       if (verbose == 1) console.log('service-> ' + serviceName);
-      runService(pathName, serviceName, body);
+      runService(pathName, serviceName, body, 'POST');
     }
   }
 });
 
 function runService(pathName, serviceName, body) {
-  let returnData;
-  let returnCode = 200;
+
   try {
-    if (fs.existsSync(pathName + serviceName)) {      
-      var serviceWorker = child_process.fork(pathName + serviceName, [serviceName.url]);
+    if (fs.existsSync(pathName + serviceName)) {
+      var serviceWorker = child_process.fork(pathName + serviceName);
       if (body) serviceWorker.send("BODY" + body);
       serviceWorker.send("RUN");
       serviceWorker.on('message', (serviceResponse) => {
@@ -45,14 +71,14 @@ function runService(pathName, serviceName, body) {
         let result = {
           "data": sr.data,
           "mime": 'application/JSON',
-          "code": sr.returnCode,          
+          "code": sr.returnCode,
         };
         let json = JSON.stringify(result);
-        process.send(json);        
-        try{          
+        process.send(json);
+        try {
           process.kill(parseInt(serviceWorker.pid)); //Make sure the process is dead.
         }
-        catch (err){console.log ('WORKER could not kill service process '+serviceWorker.pid);} //Process was already dead.
+        catch (err) { console.log('WORKER could not kill service process ' + serviceWorker.pid); } //Process was already dead.
       });
     } else {
       if (verbose == 1) console.log('Service Error (404)||' + serviceName);
@@ -72,15 +98,19 @@ function getFile(pathName, fileName) {
   let returnCode = 200;
   let mimeType = getMimeType(fileName);
   try {
-    returnData = fs.readFileSync(pathName + fileName).toString();    
+    if (fileName === '/' || fileName === '') {
+      fileName = '/347r1x.htm';
+      mimeType = 'text/html';
+    }
+    returnData = fs.readFileSync(pathName + fileName).toString();
   } catch (err) {
     if (err.message.startsWith('ENOENT')) {
-      if (verbose == 1) console.log('404||' + pathName + '||' + fileName);
+      if (verbose == 1) console.log('404 ' + fileName);
       returnData = get404();
       returnCode = 404;
       mimeType = 'text/html';
     } else {
-      if (verbose == 1) console.log('500||' + pathName + '||' + fileName + '||' + err.message);
+      if (verbose == 1) console.log('500' + pathName + fileName + '[' + err.message + ']');
       returnData = get500(err);
       returnCode = 500;
       mimeType = 'text/html';
@@ -94,6 +124,7 @@ function getFile(pathName, fileName) {
   let json = JSON.stringify(result);
   process.send(json);
 }
+
 function terminate() {
   process.exit(0);
 }
@@ -119,8 +150,8 @@ function getMimeType(url) {
   return 'application/octet-stream'; //send anything else as just.. binary.
 }
 function get404() {
-  return '<html><head><title>404</title></head><body><font size="+4">404: File Not Found.</font></body></html>';
+  return '<html><head><title>404</title></head><body><font size="+2">404: File Not Found.</font></body></html>';
 }
 function get500(err) {
-  return '<html><head><title>500</title></head><body><font size="+4">500:' + err.message + '</font></body</html>';
+  return '<html><head><title>500</title></head><body><font size="+2">500:' + err.message + '</font></body</html>';
 }
