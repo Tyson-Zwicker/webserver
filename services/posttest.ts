7a1ac43db5fbfd2ed12d7f7057e0ebf5b112b365
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-03-29 11:57:25 -0300

    improved test.

--- services/posttest.ts
+++ services/posttest.ts
@@ -1,23 +1,16 @@
-type Parameters = Record<string, string>;
-interface ServiceResult {
-  data: string;
-  returnCode: number;
-}
-interface ServiceData {
-  type: 'body' | 'parameters';
-  data: string | Parameters;
-}
+import type { Parameters, ServiceData, ServiceResult } from '.././types.ts';
 
 function isServiceData(value: unknown): value is ServiceData {
   return (typeof value === "object" && value !== null && 'type' in value && 'data' in value);
 }
 let body: string = '';
-let prams: Parameters;
+let prams: Parameters = {} as Parameters;
 
 process.on('message', (message: unknown) => {
   if (isServiceData(message)) {
     if (message.type === 'body') body = message.data as string;
     else if (message.type === 'parameters') prams = message.data as Parameters;
+    else throw new Error ('service received unknown message'+message);
   } else {
     const msg = `${message ?? ''}`;
     if (msg === 'SIGINT') {
@@ -25,8 +18,13 @@ process.on('message', (message: unknown) => {
       return;
     }
     if (msg.startsWith('RUN')) {
-      /*
-       This is where you do your thing. The rest of this just makes the service work.. 
+      /* -----------------------------------------------------------------------------
+
+      
+      This is where you do your thing. The rest of this just makes the service work.. 
+
+
+      --------------------------------------------------------------------------------
       */
       returnService(`[Data returned from service][${body}]`);
     }
